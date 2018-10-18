f435e80191ac6838081f8fcd0abdb257e3307fe3
Sdeyoung22 <43021130+Sdeyoung22@users.noreply.github.com>
2018-10-18 12:44:19 -0400

    Add files via upload

---
+++ src/MealReceipt.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class MealReceipt 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner user = new Scanner(System.in);
+		System.out.print("TAX AND TIP CALCULATOR");
+		System.out.print("Name of appetiser:  ");
+		String ap = user.nextLine();
+		System.out.print("Prince of appetiser:  ");
+		String apCost = user.nextLine();
+		System.out.print("Name of Entree:  ");
+		String en = user.nextLine();
+		System.out.print("Cost of Entree:  ");
+		String enCost = user.nextLine();
+		System.out.print("Name of Dessert:  ");
+		String de = user.nextLine();
+		System.out.print("Cost of Dessert:  ");
+		String deCost = user.nextLine();
+		System.out.print("Percent tip as decimal:  ");
+		String tip = user.nextLine();
+		
+		
+		
+		
+	}
+
+}
