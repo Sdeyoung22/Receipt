b3a74731d6dba8d93c2adc974c97f0872cb67c27
mooin <mooin@LAPTOP-NF7TBUIA.fios-router.home>
2018-10-21 20:25:31 -0400

    Added results

--- src/MealReceipt.java
+++ src/MealReceipt.java
@@ -6,25 +6,41 @@ public class MealReceipt
 	public static void main(String[] args) 
 	{
 		Scanner user = new Scanner(System.in);
-		System.out.print("TAX AND TIP CALCULATOR");
-		System.out.print("Name of appetiser:  ");
+		System.out.println("TAX AND TIP CALCULATOR");
+		System.out.print("Name of Appetiser:  ");
 		String ap = user.nextLine();
-		System.out.print("Prince of appetiser:  ");
-		String apCost = user.nextLine();
+		System.out.print("Cost of Appetiser:  ");
+		String apCostStr = user.nextLine();
 		System.out.print("Name of Entree:  ");
 		String en = user.nextLine();
 		System.out.print("Cost of Entree:  ");
-		String enCost = user.nextLine();
+		String enCostStr = user.nextLine();
 		System.out.print("Name of Dessert:  ");
 		String de = user.nextLine();
 		System.out.print("Cost of Dessert:  ");
-		String deCost = user.nextLine();
+		String deCostStr = user.nextLine();
 		System.out.print("Percent tip as decimal:  ");
-		String tip = user.nextLine();
+		String tipStr = user.nextLine();
 		
+		double apCost = Double.parseDouble(apCostStr);
+		double enCost = Double.parseDouble(enCostStr);
+		double deCost = Double.parseDouble(deCostStr);
+		double subTot = apCost+enCost+deCost;
+		double tax = subTot*0.06;
+		double tip = Double.parseDouble(tipStr);
+		double tipP = tip*100;
+		double tip2 = subTot*tip;
+		double tot = subTot+tax+tip2;
 		
-		
-		
+		System.out.println("Item\t\tPrice");
+		System.out.println("\n" + ap + "\t\t" + apCost);
+		System.out.println(en + "\t\t" + enCost);
+		System.out.println(de + "\t\t" + deCost);
+		System.out.println("\nSubtotal\t" + subTot);
+		System.out.println("Tax\t\t" + tax);
+		System.out.println("Tax\t\t" + tax);
+		System.out.println("Tip @ " + tipP + "% \t" + tip2);
+		System.out.println("Total\t\t" + tot);
 	}
 
 }
